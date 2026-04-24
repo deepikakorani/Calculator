5ba502fb8187073ec7b95642a37a89e181bc526d
deepikakorani <koranideepika2001@gmail.com>
2026-04-24 22:10:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple calculator built using HTML, CSS, and JavaScript that performs basic arithmetic operations like addition, subtraction, multiplication, and division.
---
+++ script (1).js
@@ -0,0 +1,22 @@
+let result = document.getElementById("result");
+
+function display(value){
+    result.value += value;
+}
+
+function clearScreen(){
+    result.value = "";
+}
+
+function deleteLast(){
+    result.value = result.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        result.value = eval(result.value);
+    }
+    catch{
+        result.value = "Error";
+    }
+}
